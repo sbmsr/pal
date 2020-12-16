0cc79be7dc20e1befc283b0065132de0a4668dee
r3ticuli <messs@seas.upenn.edu>
2020-12-16 18:43:55 -0500

    Initial Commit

---
+++ pal/pal.h
@@ -0,0 +1,18 @@
+//
+//  pal.h
+//  pal
+//
+//  Created by Sebastian Messier on 12/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pal.
+FOUNDATION_EXPORT double palVersionNumber;
+
+//! Project version string for pal.
+FOUNDATION_EXPORT const unsigned char palVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pal/PublicHeader.h>
+
+
